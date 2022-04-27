5ba4bac39906af6e71883c33c524a716439e3e08
rohits-dev <rshrivastava@confluent.io>
2022-04-27 18:46:56 +0100

    add cert-manager & confluent (#4)
    * fix providers
    
    * fixed readme scripts to use terraform output
    
    * add cert-manager & confluent operator

---
+++ cert-manager/input.tf
@@ -0,0 +1,56 @@
+variable "resource_prefix" {
+  default = ""
+  # validation {
+  #   condition     = length(var.resource_prefix) < 1
+  #   error_message = "The RESOURCE_PREFIX is required field."
+  # }
+}
+
+variable "root_ca_crt" {
+  default = ""
+  # validation {
+  #   condition     = length(var.root_ca_crt) < 1
+  #   error_message = "The root_ca_crt is required field."
+  # }
+}
+
+variable "root_ca_key" {
+  default = ""
+  # validation {
+  #   condition     = length(var.root_ca_key) < 1
+  #   error_message = "The root_ca_key is required field."
+  # }
+}
+
+
+variable "github_owner" {
+  type        = string
+  description = "github owner"
+  # validation {
+  #   condition     = length(var.github_owner) < 1
+  #   error_message = "The github_owner is required field."
+  # }
+}
+
+variable "repository_name" {
+  type        = string
+  default     = ""
+  description = "github repository name"
+  # validation {
+  #   condition     = length(var.repository_name) < 1
+  #   error_message = "The repository_name is required field."
+  # }
+}
+
+variable "branch" {
+  type        = string
+  default     = "main"
+  description = "branch name"
+}
+
+
+variable "target_path" {
+  type        = string
+  default     = "./"
+  description = "flux sync target path"
+}
