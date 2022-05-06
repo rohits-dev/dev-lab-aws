8b246c38c64e936fca6fa50c2984266aee4dca6f
Rohit Shrivastava <rshrivastava@confluent.io>
2022-05-06 15:22:00 +0100

    autoscaler permission

--- autoscaler/autoscaler.tf
+++ autoscaler/autoscaler.tf
@@ -8,45 +8,47 @@ resource "aws_iam_policy" "cluster_autoscaler" {
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-            {
-                "Action": [
-                    "autoscaling:DescribeAutoScalingGroups",
-                    "autoscaling:DescribeAutoScalingInstances",
-                    "autoscaling:DescribeLaunchConfigurations",
-                    "autoscaling:DescribeTags",
-                    "autoscaling:SetDesiredCapacity",
-                    "autoscaling:TerminateInstanceInAutoScalingGroup",
-                    "ec2:DescribeLaunchTemplateVersions"
-                ],
-                "Resource": "*",
-                "Effect": "Allow"
-            }
-        ]
-    })
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeTags",
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+          "ec2:DescribeLaunchTemplateVersions",
+          "ec2:DescribeInstanceTypes"
+        ],
+        "Resource" : "*",
+        "Effect" : "Allow"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "cluster_autoscaler" {
   name = "${var.resource_prefix}-cluster-autoscaler"
   assume_role_policy = jsonencode({
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "",
-                "Effect": "Allow",
-                "Principal": {
-                    "Federated": "${var.eks_oidc_provider_arn}"
-                },
-                "Action": "sts:AssumeRoleWithWebIdentity",
-                "Condition": {
-                    "StringEquals": {
-                        "${var.eks_oidc_provider}:sub": "system:serviceaccount:kube-system:cluster-autoscaler"
-                    }
-                }
-            }
-        ]
-    })
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "${var.eks_oidc_provider_arn}"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "${var.eks_oidc_provider}:sub" : "system:serviceaccount:kube-system:cluster-autoscaler"
+          }
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_autoscaler_attach" {
