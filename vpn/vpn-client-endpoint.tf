d4ff50ea3a32b9fd8039cad8b9db5898a1d2f70c
Rohit Shrivastava <rshrivastava@confluent.io>
2022-05-10 16:48:33 +0100

    remove tags from vpn

--- vpn/vpn-client-endpoint.tf
+++ vpn/vpn-client-endpoint.tf
@@ -23,23 +23,23 @@ resource "aws_security_group" "client_vpn" {
 }
 
 data "template_file" "vpn_config" {
-  template = "${file("${path.module}/vpn.config")}"
+  template = file("${path.module}/vpn.config")
   vars = {
-    ca_crt = var.root_ca_crt
-    vpn_dns = replace(aws_ec2_client_vpn_endpoint.client_vpn.dns_name, "*", "aws-lab")
+    ca_crt     = var.root_ca_crt
+    vpn_dns    = replace(aws_ec2_client_vpn_endpoint.client_vpn.dns_name, "*", "aws-lab")
     client_crt = tls_locally_signed_cert.vpn_client.cert_pem
     client_key = tls_private_key.vpn_client.private_key_pem
   }
 }
 resource "local_file" "vpn_config" {
-    content =  data.template_file.vpn_config.rendered
-    filename = "./client-vpn-config.ovpn"
+  content  = data.template_file.vpn_config.rendered
+  filename = "./client-vpn-config.ovpn"
 }
 
 resource "aws_ec2_client_vpn_endpoint" "client_vpn" {
-  
+
   description            = "${var.resource_prefix}-vpn"
-  server_certificate_arn = aws_acm_certificate.vpn_server_crt.arn 
+  server_certificate_arn = aws_acm_certificate.vpn_server_crt.arn
   client_cidr_block      = "10.1.0.0/16"
   vpc_id                 = var.vpc_id
   dns_servers            = [cidrhost(var.vpc_cidr_block, 2)]
@@ -55,15 +55,9 @@ resource "aws_ec2_client_vpn_endpoint" "client_vpn" {
   connection_log_options {
     enabled = false
   }
-
-  tags = {
-    Name  = "rohit-vpn"
-    OWNER = "rohit"
-  }
-  
 }
 
-resource "null_resource" "configure_open_vpn_client"{
+resource "null_resource" "configure_open_vpn_client" {
   depends_on = [local_file.vpn_config]
 
   provisioner "local-exec" {
