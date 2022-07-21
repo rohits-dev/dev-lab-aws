3ed4ce1a2da95715e1029be3531b997d2a7a47cf
Rohit Shrivastava <rshrivastava@confluent.io>
2022-07-21 11:54:32 +0100

    add nginx to its own namespace

--- output.tf
+++ output.tf
@@ -1,20 +1,30 @@
 output "eks_cluster_name" {
-    description = "Name of the EKS Cluster"
-    value = local.cluster_name
+  description = "Name of the EKS Cluster"
+  value       = local.cluster_name
 }
 
 output "vault_url" {
-    description = "Url of the Vault Cluster"
-    value = "vault.${var.RESOURCE_PREFIX}.local"
+  description = "Url of the Vault Cluster"
+  value       = "vault.${var.RESOURCE_PREFIX}.local"
 }
 
 output "vault_s3_bucket" {
-    description = "Name of the S3 bucket for vault"
-    value = "s3://${var.RESOURCE_PREFIX}-vault/vault/vault_secret.json"
+  description = "Name of the S3 bucket for vault"
+  value       = "s3://${var.RESOURCE_PREFIX}-vault/vault/vault_secret.json"
 }
 
 
 output "aws_region" {
-    description = "Region in which resources were deployed"
-    value = var.AWS_REGION
+  description = "Region in which resources were deployed"
+  value       = var.AWS_REGION
+}
+
+output "vpc_id" {
+  description = "vpc id"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  description = "subnet ids"
+  value       = module.vpc.private_subnets
 }
