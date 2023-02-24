11c2c5b2334b3bfc1e6060d7fd235dc425ba32d9
Rohit Shrivastava <rohit.shrivastava04@gmail.com>
2023-02-24 17:18:21 +0000

    Fix temporal issues (#26)

--- aws/eks/set-kubeconfig.tf
+++ aws/eks/set-kubeconfig.tf
@@ -2,7 +2,7 @@
 
 
 resource "null_resource" "configure_kube_config" {
-  #depends_on = [module.eks, module.vpn]
+  depends_on = [module.eks]
   provisioner "local-exec" {
     command = "aws eks update-kubeconfig --region ${var.aws_region} --name ${var.cluster_name}"
   }
