e5eef97f9b2488715410d5ac55aeb5afbfdca68c
Rohit Shrivastava <rshrivastava@confluent.io>
2023-05-19 09:36:55 +0100

    fix level-1

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  target_path      = "cluster-resources/operators/"
-  flux_target_path = "cluster-resources/flux/"
-  cluster_name     = "${var.RESOURCE_PREFIX}-eks-1"
+  target_path         = "cluster-resources/operators/"
+  target_path_level_1 = "cluster-resources/operators-level-1/"
+  flux_target_path    = "cluster-resources/flux/"
+  cluster_name        = "${var.RESOURCE_PREFIX}-eks-1"
 }
