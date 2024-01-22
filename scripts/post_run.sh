7d11cddeba0a756ce6114de753f25af1fc6908d6
Rohit Shrivastava <rshrivastava@confluent.io>
2024-01-22 15:20:25 +0000

    fix kubeconfig alias

--- scripts/post_run.sh
+++ scripts/post_run.sh
@@ -1,6 +1,6 @@
 export EKS_NAME=$(terraform output -json | jq -r '.eks_cluster_name.value | values')
 export AWS_REGION=$(terraform output -json | jq -r '.aws_region.value | values')
-aws eks update-kubeconfig --region $AWS_REGION --name $EKS_NAME
+aws eks update-kubeconfig --region $AWS_REGION --name $EKS_NAME --alias $EKS_NAME
 
 export S3_BUCKET_VAULT_OBJECT=$(terraform output -json | jq -r '.vault_s3_bucket.value | values')
 
