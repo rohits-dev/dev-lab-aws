c897c2049da31975e6b7a515c33d4a827bea1454
Rohit Shrivastava <rohit.shrivastava04@confluent.io>
2024-08-28 21:32:49 +0000

    fix helm provider

--- provider.tf
+++ provider.tf
@@ -109,7 +109,7 @@ provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
       command     = "aws"
